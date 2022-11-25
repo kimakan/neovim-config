b18fcb5384bd0d54008c0f936a4070cf0a884a09
kimakan <kmakan@aip.de>
2022-11-25 23:18:44 +0100

    add simple install script with some notes

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -89,7 +89,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 vim.keymap.set('n', '<A-e>', vim.diagnostic.open_float, opts)
 require('lspconfig')['pyright'].setup {
   capabilities = capabilities
---
+++ install-config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo snap install nvim --classic
+
+if ! command -v npm &> /dev/null
+then
+    sudo apt install curl
+fi
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+cp -r config/nvim ~/.config/
+
+npm install pyright
+
+mkdir -p ~/.local/bin
+curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
+
+# ./local/bin to PATH
+
+
+
