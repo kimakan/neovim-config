8426a97d1d12cd6ab599edbf59c5018f60a611a2
Kirill Makan <kmakan@aip.de>
2023-08-22 12:54:23 +0200

    fix repo cloning for already existing folder

--- install.sh
+++ install.sh
@@ -3,11 +3,16 @@ rm -rf $HOME/.config/nvim
 cp -r nvim $HOME/.config/
 
 # Install the packer from https://github.com/wbthomason/packer.nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+path_to_packer="${HOME}/.local/share/nvim/site/pack/packer/start/packer.nvim"
+tmp_path="/tmp/packer.nvim"
+
+rm -rf ${path_to_packer}
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ${tmp_path}
+cp -r ${tmp_path} ${path_to_packer}
+rm -rf ${tmp_path}
 
 
-echo "Open .config/nvim/lua/custom/packer.lua in NVIM"
+echo "Open ${HOME}/.config/nvim/lua/custom/packer.lua in NVIM"
 echo "Run ':so'"
 echo "Run ':PackerSync'"
 echo "Run ':TSUpdate'"
