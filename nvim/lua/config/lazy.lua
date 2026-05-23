81be49c5b20a2aae247506162ef4df3eec67c0a6
kimakan <45099849+kimakan@users.noreply.github.com>
2026-05-23 16:35:30 +0200

    Add gitui

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.toml" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.util.gitui" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
